ad82a25077f4a3fd8223c087a4017b2262a21b40
Daniel Nolan <jokerjam@users.noreply.github.com>
2019-08-11 19:46:41 +0100

    create vnet in north eruope using cli

--- Virtual-Networks/create-vnet.sh
+++ Virtual-Networks/create-vnet.sh
@@ -1,14 +1,24 @@
-company = "JOK"
-product = "JAM"
-env = "DEV"
-instance = "01"
-region = "EUW"
-location = "westeurope"
+company="JOK"
+product="JAM"
+env="DEV"
+instance="01"
+region="EUN"
+location="northeurope"
+
+# Create resource group name
+rgName=$company-$product-$env-$instance-$region
+echo $rgName
 
 # login
 az login
-# Show tags on resource group
-az group show -n $rgName --query tags
+
+# Create resource group
+az group create --name $rgName --location $location --tags 'env='$env
+# Show resource group
+az group show -n $rgName
 
 # Create a vnet
-az network vnet create
+vnetName=$rgName-"VNT"
+addrPrefix="10.1.0.0/17"
+echo $vnetName
+az network vnet create --name $vnetName --resource-group $rgName --address-prefixes $addrPrefix
