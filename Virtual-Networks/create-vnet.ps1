791efeb07ac81ce903be6ec984ec8ce7af014f20
Daniel Nolan <jokerjam@users.noreply.github.com>
2019-08-11 18:39:26 +0100

    create resource group

--- Virtual-Networks/create-vnet.ps1
+++ Virtual-Networks/create-vnet.ps1
@@ -6,10 +6,14 @@ $tenant = Read-Host -Prompt 'Input your Azure tenant'
 Connect-AzAccount -Tenant $tenant -SubscriptionName "Visual Studio Enterprise"
 
 # Set Environment variables
-$company = "JOk"
+$company = "JOK"
 $product = "JAM"
 $env = "DEV"
 $instance = "01"
 $region = "EUW"
 $location = "westeurope"
 
+# Create resource group
+$rgName = "{0}-{1}-{2}-{3}-{4}" -f $company, $product, $env, $instance, $region
+Write-Output "Creating Resource Group - $($rgName)"
+New-AzResourceGroup -Name $rgName -Location $location
